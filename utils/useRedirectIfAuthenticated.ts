001a115a484cac4f1a882bb569d7cda6209915dc
Dulnyarat9458 <dulnyarat.9458@gmail.com>
2025-07-05 01:11:09 +0700

    fix: fix redirect loop

--- utils/useRedirectIfAuthenticated.ts
+++ utils/useRedirectIfAuthenticated.ts
@@ -1,15 +1,26 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { usePathname, useRouter } from "next/navigation";
 import { anonymousPath } from "@/constants/anonymousPath";
 
 export function useRedirectIfAuthenticated(userInfo: { email?: string }) {
   const router = useRouter();
   const pathname = usePathname();
+  const prevEmail = useRef<string | undefined>(undefined);
 
   useEffect(() => {
-    if (userInfo && userInfo.email && anonymousPath.includes(pathname)) {
-      router.push("/");
+    if (
+      userInfo &&
+      typeof userInfo.email === 'string' &&
+      userInfo.email.length > 0 &&
+      anonymousPath.includes(pathname) &&
+      pathname !== '/' &&
+      prevEmail.current !== userInfo.email
+    ) {
+      prevEmail.current = userInfo.email;
+      router.push('/');
+    } else {
+      prevEmail.current = userInfo?.email;
     }
   }, [userInfo, pathname, router]);
 }
